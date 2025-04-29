40f257c320d778eecc92746dbe8b7c63be0cf9fa
h19josan <143787220+h19josan@users.noreply.github.com>
2025-04-29 13:36:05 +0200

    list
    list

--- HashTableLab/HashTableLab/ListHashTable.cs
+++ HashTableLab/HashTableLab/ListHashTable.cs
@@ -8,32 +8,63 @@ namespace HashTableLab
 {
     internal class ListHashTable <TKey, TValue> : IHashTable<TKey, TValue>
     {
+        private readonly List<KeyValuePair<TKey, TValue>>[] _buckets;
         private readonly int _size;
         public ListHashTable(int size = 10) 
         {
             this._size = size;
+            _buckets = new List<KeyValuePair<TKey, TValue>>[_size];
+
+            for (int i = 0; i < _size; i++)
+            {
+                _buckets[i] = new List<KeyValuePair<TKey, TValue>>();
+            }
         }
 
-        public int Size { get { return _size; } }
+        public int Size => _size;
 
         public void Add(TKey key, TValue value)
         {
-            throw new NotImplementedException();
+            var bucket = _buckets[GetBucketIndex(key)];
+            if (bucket.Any(kvp => kvp.Key.Equals(key)))
+            
+                throw new ArgumentException("Key already exists in the hash table.");
+            
+            bucket.Add(new KeyValuePair<TKey, TValue>(key, value));
         }
 
         public TValue Get(TKey key)
         {
-            throw new NotImplementedException();
+            var bucket = _buckets[GetBucketIndex(key)];
+            foreach (var kvp in bucket)
+            {
+                if (kvp.Key.Equals(key))
+                {
+                    return kvp.Value;
+                }
+            }
+            throw new KeyNotFoundException("Key not found in the hash table.");
         }
 
         public void Remove(TKey key)
         {
-            throw new NotImplementedException();
+            var bucket = _buckets[GetBucketIndex(key)];
+            var index = bucket.FindIndex(kvp => kvp.Key.Equals(key));
+
+            if (index >= 0)
+            {
+                bucket.RemoveAt(index);
+            }
+            else
+            {
+                throw new KeyNotFoundException("Key not found in the hash table.");
+            }
         }
 
         public bool ContainsKey(TKey key)
         {
-            throw new NotImplementedException();
+            var bucket = _buckets[GetBucketIndex(key)];
+            return bucket.Any(kvp => kvp.Key.Equals(key));
         }
 
         private int GetBucketIndex(TKey key)
