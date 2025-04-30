963894706339f10c8388b09e3cc52f3cb1aa91db
h23maarv <h23maarv@du.se>
2025-04-30 10:58:17 +0200

    Skapat Probing-klass

---
+++ HashTableLab/HashTableOpenAddressing/QuadraticProbingHashTable.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableOpenAddressing
+{
+    public class QuadraticProbingHashTable<TKey, TValue> : IHashTable<TKey, TValue>
+    {
+        private KeyValuePair<TKey, TValue>?[] _buckets;
+        private int _size;
+        private int _count;
+        private static readonly double DEFAULT_LOAD_FACTOR = 0.7;
+
+        public QuadraticProbingHashTable(int size = 10)
+        {
+            if (size <= 0)
+                throw new ArgumentException("Size must be positive.", nameof(size));
+
+            this._size = size;
+            this._buckets = new KeyValuePair<TKey, TValue>?[this._size];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (key == null)
+                throw new ArgumentNullException(nameof(key));
+
+            if (this._count >= this._size * DEFAULT_LOAD_FACTOR)
+                this.Resize();
+
+            int index = this.GetIndex(key);
+            int i = 0;
+            int firstTombstone = -1;
+
+            while (this._buckets[index].HasValue)
+            {
+                if (this._buckets[index].Value.Key != null && this._buckets[index].Value.Key.Equals(key))
+                {
+                    throw new ArgumentException($"Key '{key}' already exists.");
+                }
+                i++;
+                index = (index + i * i) % this._size;
+            }
+
+            this._buckets[index] = new KeyValuePair<TKey, TValue>(key, value);
+            this._count++;
+
+            if (firstTombstone != -1)
+            {
+                index = firstTombstone;
+            }
+
+            this._buckets[index] = new KeyValuePair<TKey, TValue>(key, value);
+            this._count++;
+        }
+
+        public TValue Get(TKey key)
+        {
+            if (key == null)
+                throw new ArgumentNullException(nameof(key));
+
+            int index = this.GetIndex(key);
+            int i = 0;
+
+            while (this._buckets[index].HasValue)
+            {
+                if (this._buckets[index].Value.Key != null && this._buckets[index].Value.Key.Equals(key))
+                {
+                    return this._buckets[index].Value.Value;
+                }
+                i++;
+                index = (index + i * i) % this._size;
+            }
+            throw new KeyNotFoundException($"Key '{key}' not found.");
+        }
+
+        public void Remove(TKey key)
+        {
+            if (key == null)
+                throw new ArgumentNullException(nameof(key));
+
+            int index = this.GetIndex(key);
+            int i = 0;
+
+            while (this._buckets[index].HasValue)
+            {
+                if (this._buckets[index].Value.Key != null && this._buckets[index].Value.Key.Equals(key))
+                {
+                    this._buckets[index] = new KeyValuePair<TKey, TValue>(default, default); ;
+                    this._count--;
+                    return;
+                }
+
+                i++;
+                index = (index + i * i) % this._size;
+            }
+            throw new KeyNotFoundException($"Key '{key}' not found.");
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            if (key == null)
+                throw new ArgumentNullException(nameof(key));
+
+            int index = this.GetIndex(key);
+            int i = 0;
+
+            while (this._buckets[index].HasValue)
+            {
+                if (this._buckets[index].Value.Key != null && this._buckets[index].Value.Key.Equals(key))
+                {
+                    return true;
+                }
+
+                i++;
+                index = (index + i * i) % this._size;
+            }
+            return false;
+        }
+
+        private int GetIndex(TKey key)
+        {
+            return Math.Abs(key.GetHashCode()) % this._size;
+        }
+
+        private void Resize()
+        {
+            var oldBuckets = this._buckets;
+            this._size *= 2;
+            this._buckets = new KeyValuePair<TKey, TValue>?[_size];
+            this._count = 0;
+
+            foreach (var bucket in oldBuckets)
+            {
+                if (bucket.HasValue)
+                {
+                    int index = Math.Abs(bucket.Value.Key.GetHashCode()) % _size;
+                    int i = 0;
+                    while (_buckets[index].HasValue)
+                    {
+                        i++;
+                        index = (index + i * i) % _size;
+                    }
+                    _buckets[index] = bucket;
+                }
+            }
+        }
+    }
+}
